44fff1df17d0e82b5a759d9b3867f299adcf2dfe
sherlockmatt <matthew.w.dawson@gmail.com>
2018-12-01 14:42:32 +0000

    Improved performance of puzzle 2 by nearly 20000%

--- day1.py
+++ day1.py
@@ -31,6 +31,26 @@ def puzzle2():
     print('Answer is: {}, found at index: {}'.format(ans, index))
 
 
+def puzzle2_faster():
+    ans = False
+    current = 0
+    deltas = getData()
+    freqs = set()
+
+    numdeltas = len(deltas)
+    index = 0
+    while not ans and index < 1000000:  # We should find it within 1 million checks, riiiight?
+        current += deltas[index % numdeltas]
+        # This test is much faster for sets than lists
+        if current in freqs:
+            ans = current
+        else:
+            freqs.add(current)
+        index += 1
+
+    print('Answer is: {}, found at index: {}'.format(ans, index))
+
+
 if __name__ == '__main__':
     puzzle1()
-    puzzle2()
+    puzzle2_faster()
