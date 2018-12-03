10e52e2ada1b97ce69fe39f3cf8d977a37b74ae6
sherlockmatt <matthew.w.dawson@gmail.com>
2018-12-03 08:15:55 +0000

    Added template to make starting new days easier

---
+++ template.py
@@ -0,0 +1,20 @@
+from helpers import download
+
+
+def getData():
+    r = download('https://adventofcode.com/2018/day/2/input')
+    return list(r.iter_lines())
+
+
+def puzzle1(data):
+    print('Answer: {}'.format(data))
+
+
+def puzzle2(data):
+    print('Answer: {}'.format(data))
+
+
+if __name__ == '__main__':
+    inputData = getData()
+    puzzle1(inputData)
+    puzzle2(inputData)
